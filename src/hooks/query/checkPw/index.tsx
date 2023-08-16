99d108ad587c6a548dd783345cb55bee3bb2370d
choi138 <kidjustinchoi@gmail.com>
2023-08-16 17:23:17 +0900

    fix: check pw error handling fix

--- src/hooks/query/checkPw/index.tsx
+++ src/hooks/query/checkPw/index.tsx
@@ -15,14 +15,24 @@ export const useCheckPw = (): UseMutationResult<
 > => {
   const navigation = useNavigate();
   const setCheckPw = useSetRecoilState(checkPwState);
-  return useMutation('useCheckPw', checkPw, {
-    onSuccess: ({ token }, variables) => {
-      setCheckPw({ token: token, isEncrypt: true });
-      navigation(`/dl/${variables.id}`);
+  return useMutation(
+    'useCheckPw',
+    ({ id, password }) => {
+      if (id === '' || id === undefined) {
+        toastError('잘못된 접근입니다.');
+        navigation('/');
+      }
+      return checkPw({ id, password });
     },
-    onError: () => {
-      toastError('비밀번호가 틀렸어요.');
+    {
+      onSuccess: ({ token }, variables) => {
+        setCheckPw({ token: token, isEncrypt: true });
+        navigation(`/dl/${variables.id}`);
+      },
+      onError: () => {
+        toastError('비밀번호가 틀렸어요.');
+      },
+      retry: 0,
     },
-    retry: 0,
-  });
+  );
 };
