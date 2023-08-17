52bb3ce738a84d433893be35b42eccb29b83533b
n ghtcjm <84369321+choi138@users.noreply.github.com>
2023-08-17 13:56:18 +0900

    fix: del main component

--- src/components/main/expireTime/styled.ts
+++
@@ -1,6 +0,0 @@
-import styled from '@emotion/styled';
-
-export const ExpireTimeOptionContainer = styled.div`
-  display: flex;
-  justify-content: space-between;
-`;
--- src/components/main/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './Button';
-export * from './DownloadSlider';
-export * from './ExpireTime';
-export * from './OptionSection';
